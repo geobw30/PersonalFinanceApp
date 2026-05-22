ed46a3a6a4a8d4d18ee5dead238d805e58da2c02
gbarugahare <gbarugahare@idi.co.ug>
2026-05-22 16:13:47 +0300

    testing connectivity to sqlserver on Azure from the Heroku container

---
+++ AzureTest/AzureTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+var connectionString = "Server=tcp:mssql-azure-pfa.database.windows.net,1433;" +
+                               "Initial Catalog=PersonalFinanceDb;" +
+                               "Persist Security Info=False;" +
+                               "User ID=geobw30;" +
+                               "Password=3N73ri2e-;" +
+                               "MultipleActiveResultSets=False;" +
+                               "Encrypt=True;" +
+                               "TrustServerCertificate=False;" +
+                               "Connection Timeout=30;";
+
+try
+{
+    using (var conn = new SqlConnection(connectionString))
+    {
+        conn.Open();
+        Console.WriteLine("Connected successfully!");
+
+        using (var cmd = new SqlCommand("SELECT TOP 1 name FROM sys.databases", conn))
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine($"Database: {reader.GetString(0)}");
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Connection failed: " + ex.Message);
+}
